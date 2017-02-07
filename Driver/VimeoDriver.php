58474d20a1b1b9542fcf834fda58c2c64dec752b
mpoletti <mpoletti@kangourouge.com>
2017-02-07 15:18:39 +0100

    add cache for thumbnail

--- Driver/VimeoDriver.php
+++ Driver/VimeoDriver.php
@@ -6,78 +6,108 @@ use Vimeo\Vimeo;
 use EMC\FileinputBundle\Entity\FileInterface;
 
 class VimeoDriver implements DriverInterface {
-    
-    /**
-     * @var Vimeo
-     */
-    private $vimeo;
 
-    /**
-     * @var array
-     */
-    private $settings;
-    
-    /**
-     * @var array
-     */
-    private $whitelist;
-    
-    /**
-     * @var array
-     */
-    static private $cache = array();
-    
-    function __construct($clientId, $clientSecret, $accessToken, array $settings, array $whitelist) {
-        $this->vimeo = new Vimeo($clientId, $clientSecret);
-        $this->vimeo->setToken($accessToken);
-        $this->settings = $settings;
-        $this->whitelist = $whitelist;
-    }
-    
-    public function upload($pathname, array $settings) {
-        $video = $this->vimeo->upload($pathname, false);
-        
-        if (!preg_match('`/videos/[0-9]+`', $video)) {
-            throw new \Exception('Unable to upload file.');
-        }
+	/**
+	 * @var Vimeo
+	 */
+	private $vimeo;
 
-        $settings = array_merge_recursive($this->settings, $settings);
-        $response = $this->vimeo->request($video, $settings ?: $this->settings, 'PATCH');
-        foreach($this->whitelist as $domain) {
-            $response = $this->vimeo->request(sprintf('%s/privacy/domains/%s', $video, $domain), array(), 'PUT');
-        }
-        
-        return $video;
-    }
-    
-    public function get($video) {
-        if (!isset(self::$cache[$video])) {
-            $data = $this->vimeo->request($video);
-            if ($data['status'] !== 200) {
-                return null;
-            }
-            self::$cache[$video] = $data['body'];
-        }
-        return self::$cache[$video];
-    }
-    
-    public function delete($video) {
-        return $this->vimeo->request($video, array(), 'DELETE');
-    }
+	/**
+	 * @var array
+	 */
+	private $settings;
 
-    public function getUrl($pathname) {
-        $data = $this->get($pathname);
-        return $data ? $data['link'] : null;
-    }
+	/**
+	 * @var array
+	 */
+	private $whitelist;
 
-    public function getThumbnail($pathname) {
-        $data = $this->get($pathname);
-        return $data ? $data['pictures']['sizes'][3]['link_with_play_button'] : null;
-    }
-    
-    public function render($pathname) {
-        $data = $this->get($pathname);
-        return $data ? $data['embed']['html'] : null;
-    }
+	/**
+	 * @var string
+	 */
+	private $kernelRootDir;
+
+	/**
+	 * @var string
+	 */
+	private $cacheDir;
+
+	/**
+	 * @var array
+	 */
+	static private $cache = array();
+
+	function __construct($clientId, $clientSecret, $accessToken, array $settings, array $whitelist, $kernelRootDir, $cacheDir) {
+		$this->vimeo = new Vimeo($clientId, $clientSecret);
+		$this->vimeo->setToken($accessToken);
+		$this->settings = $settings;
+		$this->whitelist = $whitelist;
+		$this->kernelRootDir = $kernelRootDir;
+		$this->cacheDir = $cacheDir;
+	}
+
+	public function upload($pathname, array $settings) {
+		$video = $this->vimeo->upload($pathname, false);
+
+		if (!preg_match('`/videos/[0-9]+`', $video)) {
+			throw new \Exception('Unable to upload file.');
+		}
+
+		$settings = array_merge_recursive($this->settings, $settings);
+		$response = $this->vimeo->request($video, $settings ?: $this->settings, 'PATCH');
+		foreach($this->whitelist as $domain) {
+			$response = $this->vimeo->request(sprintf('%s/privacy/domains/%s', $video, $domain), array(), 'PUT');
+		}
+
+		return $video;
+	}
+
+	public function get($video) {
+
+		if (!isset(self::$cache[$video])) {
+			$data = $this->vimeo->request($video);
+			if ($data['status'] !== 200) {
+				return null;
+			}
+			self::$cache[$video] = $data['body'];
+		}
+		return self::$cache[$video];
+	}
+
+	public function delete($video) {
+		return $this->vimeo->request($video, array(), 'DELETE');
+	}
+
+	public function getUrl($pathname) {
+		$data = $this->get($pathname);
+		return $data ? $data['link'] : null;
+	}
+
+	public function getThumbnail($pathname) {
+		$path = sprintf('%s/../web%s%s', $this->kernelRootDir, $this->cacheDir, $pathname);
+		$url = $this->cacheDir.$pathname;
+
+		if(file_exists($path)){
+			return $url;
+		}
+
+		$data = $this->get($pathname);
+
+		if($data){
+			$link = $data['pictures']['sizes'][3]['link'];
+
+			if(!copy($link, $path)){
+				return $link;
+			}
+			return $url;
+		}
+
+		return null;
+	}
+
+	public function render($pathname) {
+		$data = $this->get($pathname);
+		return $data ? $data['embed']['html'] : null;
+	}
 
 }
